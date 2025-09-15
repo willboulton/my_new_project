3ccf0d26c4e417786010d3324256f0d3131d92ec
willboulton <wboulton12@gmail.com>
2025-09-15 11:50:59 +0100

    fixed the println cat bug

--- my_file.R
+++ my_file.R
@@ -1,5 +1,10 @@
 library('hello')
 
-println("This is my R code")
-
+cat("This is my R code")
+# I added this comment over the last week
 println("Imagine that it performs several tasks")
+
+# Here is another important line
+
+# My new code is here which took time to develop
+cat("Hi")
